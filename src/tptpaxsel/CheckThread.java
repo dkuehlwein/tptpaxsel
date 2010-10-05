a3444215601d0dc80857a09417e686e2464fa894
Creszent <Creszent@a98bb640-1ba4-07c4-5154-d3010dba468e>
2010-10-05 14:59:50 +0000

    Greatly improved multithreding. Added locks for guaranteed(...) consistency.

--- src/tptpaxsel/CheckThread.java
+++ src/tptpaxsel/CheckThread.java
@@ -2,6 +2,7 @@ package tptpaxsel;
 
 import java.util.Vector;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.io.*;
 
 import tptpaxsel.Obligation;
@@ -12,7 +13,7 @@ import org.jgrapht.graph.DefaultWeightedEdge;
 
 class CheckThread extends Thread {
 	
-	static ArrayList<Thread> Threads = new ArrayList<Thread>();
+	static ArrayList<CheckThread> Threads = new ArrayList<CheckThread>();
 
 	static Obligation obligation;
 	static ObligationStatistics stats;
@@ -30,6 +31,7 @@ class CheckThread extends Thread {
 	
 	public static int gAxiomCounter=0;
 	
+	public static HashMap<String,Process> provers;
 	
 
 	public static boolean checkLoop(Obligation obl, int numThreads, double weightGraph, PrintStream oStream,
@@ -45,7 +47,9 @@ class CheckThread extends Thread {
 		
 		gAxiomCounter = 0;
 		
-		Threads = new ArrayList<Thread>();
+		Threads = new ArrayList<CheckThread>();
+		
+		provers = new HashMap<String,Process>();
 		
 		ArrayList<Vector<CheckSetting>> checkSettings = new ArrayList<Vector<CheckSetting>>();
 		for(int i=0;i<numThreads;i++){
@@ -68,19 +72,37 @@ class CheckThread extends Thread {
 			Threads.add(new CheckThread(checkSettings.get(i)));
 			Threads.get(i).start();
 		}
+		
 		try{
 			Threads.get(0).join();
 		}
 		catch(InterruptedException e){}
 		return checkResult;
 	}
-
-	public static void terminateAll(){
-		for(Thread t: Threads)
-			t.interrupt();
+	static public void terminateAll(CheckThread caller){
+		for(CheckThread t: Threads){
+			if(t==caller) continue;
+			synchronized(t.lock){
+				if(!t.isInterrupted())
+					try{
+						t.lock.wait();
+					}
+					catch(InterruptedException e){
+						e.printStackTrace();
+					}
+				t.interrupt();
+				if(provers.containsKey(t.getName())){
+					provers.get(t.getName()).destroy();
+					provers.remove(t.getName());
+				}
+			}
+		}
 	}
 	
 	Vector<CheckSetting> checkSettings;
+	
+	public Object lock = new Object();
+	
 	
 	CheckThread(Vector<CheckSetting> checkSettings){
 		this.checkSettings = checkSettings;
@@ -88,53 +110,47 @@ class CheckThread extends Thread {
 
 	public void run(){
 		double checkTryTime;
-		while(!isInterrupted()){
-			for (CheckSetting checkSetting : checkSettings) {
-				int givenAxiomCounter;
-				checkTryTime = System.currentTimeMillis();
-				/* Statistics Start */
-				stats.setProofTries(stats.getProofTries()+1);
-				stats.addProver(checkSetting.prover);
-				/* Statistics End */
-				if (checkSetting.numOfPrem == -1) {
-					givenAxiomCounter = obligation.premises.size(); 
-				} else {
-					givenAxiomCounter = (int)Math.floor(checkSetting.numOfPrem+0.5d);
-				}
-				/* Select the right premises */				
-				try {					
-					Obligations.createTPTPProblem(obligation, givenAxiomCounter);
-				} catch (IOException e) {
-					outStream.println("Could not create TPTP Problem File");
-					e.printStackTrace();
-				}				
-				
-				/* Create the proof process */
-				ProcessBuilder proverProcess;
-				if (checkSetting.prover.equals("V")) {
-					proverProcess = new ProcessBuilder(							
-							"lib/VAMPIRE/vampire_lin32",
-							"-t",Integer.toString(checkSetting.time), 							
-							"--proof","tptp", 
-							"--memory_limit","1500", 
-							"--output_axiom_names","on",
-							"--mode","casc", 
-							"--input_file",
-							obligation.ATPInput.toString()); 								
-				} else if (checkSetting.prover.equals("E10")) { 
-					proverProcess = new ProcessBuilder(
-							"/home/rekzah/Programming/Naproche/Naproche-Due/naproche_core/www/cgi-bin/TPTP/Systems/EP---1.0/eproof",							
-							"-xAuto",
-							"-tAuto",
-							"--cpu-limit="+checkSetting.time,
-							"--proof-time-unlimited",
-							"--memory-limit=Auto",
-							"--tstp-in",
-							"--tstp-out",
-							obligation.ATPInput.toString());						
-				} else { 
-					proverProcess = new ProcessBuilder(						
-						"eproof",
+		boolean cResult=false;
+		for (CheckSetting checkSetting : checkSettings) {
+			synchronized(lock){
+				lock.notifyAll();
+			}
+			if(isInterrupted()) break;
+			
+			int givenAxiomCounter;
+			checkTryTime = System.currentTimeMillis();
+			/* Statistics Start */
+			stats.setProofTries(stats.getProofTries()+1);
+			stats.addProver(checkSetting.prover);
+			/* Statistics End */
+			if (checkSetting.numOfPrem == -1) {
+				givenAxiomCounter = obligation.premises.size(); 
+			} else {
+				givenAxiomCounter = (int)Math.floor(checkSetting.numOfPrem+0.5d);
+			}
+			/* Select the right premises */				
+			try {					
+				Obligations.createTPTPProblem(obligation, givenAxiomCounter);
+			} catch (IOException e) {
+				outStream.println("Could not create TPTP Problem File");
+				e.printStackTrace();
+			}				
+			
+			/* Create the proof process */
+			ProcessBuilder proverProcess;
+			if (checkSetting.prover.equals("V")) {
+				proverProcess = new ProcessBuilder(							
+						"lib/VAMPIRE/vampire_lin32",
+						"-t",Integer.toString(checkSetting.time), 							
+						"--proof","tptp", 
+						"--memory_limit","1500", 
+						"--output_axiom_names","on",
+						"--mode","casc", 
+						"--input_file",
+						obligation.ATPInput.toString()); 								
+			} else if (checkSetting.prover.equals("E10")) { 
+				proverProcess = new ProcessBuilder(
+						"/home/rekzah/Programming/Naproche/Naproche-Due/naproche_core/www/cgi-bin/TPTP/Systems/EP---1.0/eproof",							
 						"-xAuto",
 						"-tAuto",
 						"--cpu-limit="+checkSetting.time,
@@ -142,52 +158,100 @@ class CheckThread extends Thread {
 						"--memory-limit=Auto",
 						"--tstp-in",
 						"--tstp-out",
-						obligation.ATPInput.toString());
+						obligation.ATPInput.toString());						
+			} else { 
+				proverProcess = new ProcessBuilder(						
+					"/usr/local/bin/eproof",
+					"-xAuto",
+					"-tAuto",
+					"--cpu-limit="+checkSetting.time,
+					"--proof-time-unlimited",
+					"--memory-limit=Auto",
+					"--tstp-in",
+					"--tstp-out",
+					obligation.ATPInput.toString());
+			}
+			
+			/* Run the Prover */						
+			try {					
+				synchronized(lock){
+					lock.notifyAll();
 				}
+				if(isInterrupted()) break;
 				
-				/* Run the Prover */						
-				try {					
-					Process runProver = proverProcess.start();
-					// Set up the in and output streams				
-					InputStream is = runProver.getInputStream();
-					InputStreamReader isr = new InputStreamReader(is);
-					BufferedReader br = new BufferedReader(isr);
-					FileWriter fstream = new FileWriter(obligation.ATPOutput);
-					BufferedWriter out = new BufferedWriter(fstream);
-					ATPOutputParser ATPParser = new ATPOutputParser(br, out);
-					// Parse the stream for results		
-					checkResult = ATPParser.parse(graph, conjecture, weightUsedGraph);
+				Process runProver = proverProcess.start();
+				provers.put(this.getName(),runProver);
+				// Set up the in and output streams				
+				InputStream is = runProver.getInputStream();
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader br = new BufferedReader(isr);
+				FileWriter fstream = new FileWriter(obligation.ATPOutput);
+				BufferedWriter out = new BufferedWriter(fstream);
+				ATPOutputParser ATPParser = new ATPOutputParser(br, out);
+				
+				// Parse the stream for results		
+				
+				if(isInterrupted()){
 					is.close();
 					out.close();
-					runProver.destroy();
-					outStream.print(" "+checkResult+"("+checkSetting.prover+")");
-					/* If we found a proof, we can stop */
-					if (checkResult) {
-						CheckThread.gAxiomCounter = givenAxiomCounter;
-						/* Statistics Start */
-						terminateAll();
-						obligation.checkResult = checkResult;
-						stats.setUsedAxiomsNumber(ATPParser.usedAxioms.size());
-						stats.setUsedAxioms(ATPParser.usedAxioms);
-						stats.setProofTime((System.currentTimeMillis() - checkTryTime)/1000);
-						stats.setProver(checkSetting.prover);
-						stats.setInconsistencyWarning(ATPParser.inconsistencyWarning);
-						obligation.inconsistencyWarning = ATPParser.inconsistencyWarning;
-						for (int i = 0; i < stats.getUsedAxiomsNumber(); i++) {
-							Axiom axiom = ATPParser.usedAxioms.elementAt(i);
-							int localRelevance = obligation.premises.indexOf(axiom)+1;							
-							stats.setMaxDistance(Math.max(localRelevance, stats.getMaxDistance()));							
-						}
-						/* Statistics End */
-						break;
-					} 										
-				} catch (IOException e) {
-					outStream.println("Could not run prover");
-					e.printStackTrace();
-					this.interrupt();
+					break;
 				}
+				
+				synchronized(lock){
+					lock.notifyAll();
+				}
+				cResult = ATPParser.parse(graph, conjecture, weightUsedGraph);
+				synchronized(lock){
+					lock.notifyAll();
+				}
+				is.close();
+				out.close();
+				runProver.destroy();
+				provers.remove(this.getName());
+				
+				if(isInterrupted()) break;
+				
+				outStream.print(" "+cResult+"("+checkSetting.prover+")");
+				/* If we found a proof, we can stop */
+				if (cResult) {
+					synchronized(lock){
+						lock.notifyAll();
+					}
+					if(isInterrupted()) break;
+					CheckThread.gAxiomCounter = givenAxiomCounter;
+					/* Statistics Start */
+					CheckThread.terminateAll(this);
+					checkResult = cResult;
+					obligation.checkResult = checkResult;
+					stats.setUsedAxiomsNumber(ATPParser.usedAxioms.size());
+					stats.setUsedAxioms(ATPParser.usedAxioms);
+					stats.setProofTime((System.currentTimeMillis() - checkTryTime)/1000);
+					stats.setProver(checkSetting.prover);
+					stats.setInconsistencyWarning(ATPParser.inconsistencyWarning);
+					obligation.inconsistencyWarning = ATPParser.inconsistencyWarning;
+					for (int i = 0; i < stats.getUsedAxiomsNumber(); i++) {
+						Axiom axiom = ATPParser.usedAxioms.elementAt(i);
+						int localRelevance = obligation.premises.indexOf(axiom)+1;							
+						stats.setMaxDistance(Math.max(localRelevance, stats.getMaxDistance()));							
+					}
+					/* Statistics End */
+					synchronized(lock){
+						lock.notifyAll();
+					}
+					this.interrupt();
+					break;
+				} 										
+			} catch (IOException e) {
+				outStream.println("Could not run prover");
+				e.printStackTrace();
+				synchronized(lock){
+					lock.notifyAll();
+				}
+				this.interrupt();
 			}
-			this.interrupt();
+			synchronized(lock){
+				lock.notifyAll();
+			}
 		}
 	}
 }
