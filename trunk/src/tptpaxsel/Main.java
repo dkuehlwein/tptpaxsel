34898e3fcb35086b8c4837171189d3e620c9dd77
Creszent <Creszent@a98bb640-1ba4-07c4-5154-d3010dba468e>
2010-10-05 14:59:50 +0000

    Greatly improved multithreding. Added locks for guaranteed(...) consistency.

--- trunk/src/tptpaxsel/Main.java
+++ trunk/src/tptpaxsel/Main.java
@@ -35,50 +35,43 @@ public class Main {
 	 */	
 	public static void main(String[] args) {
 
-		AtpApi api = new AtpApi("/examples/landau/");
-		api.setSimple(0.0,0.0,1.0, "V", 3, 2);
-		api.runAll();
-		/* Example example;
-		Obligations obligations;
-		PSASimple one;
-		PSASimple two;
-		PSAPremiseGrowth premiseGrowth;
-		PSADivvy divvy;
-		PSAExistential ex = new PSAExistential();
-		
-		/* Copy pasteable foobar 
-		String[] provers = new String[2];
-		provers[0] = "V";
-		provers[1] = "E";
-		premiseGrowth = new PSAPremiseGrowth(5, "plus", 10, 5, 0, provers);
-		divvy = new PSADivvy(2, 5, provers, true);
-		one = new PSASimple("V",300);
-		two = new PSASimple("E",5);
-		/* End Copy pasteable foobar */
-		
-		
-		/* Start tests 
-        example = new Example("Euclid");
-		//example.runAprils();
-		obligations = example.obligations;		
-		ex.deleteExistentialPremises(obligations);
-		//Default values are weightAPRILS = 1; weightObligationGraph=1; maxTime = 5 * 1000		
-		obligations.weightAPRILS = 0.0;
-		obligations.weightNaproche = 1.0;
-		obligations.weightObligationGraph = 1.0;
-		obligations.maxTime = maxTime;
-		//PSA Settings
-		divvy = new PSADivvy(2, 5, provers, true);		
-		// Log
-		System.out.println("Example: "+example.name);
-		divvy.changeCheckSettings(obligations);
-		obligations.checkObligations();
-		/* Write machine readable stats Start 
-		Statistics.printMachineStats(obligations, "EuclidDivvyEV5N1A0.stats");
-		Statistics.printProcessTimeline(obligations, 300, "EuclidDivvyEV5N1A0.timeline");
-		/* Write machine readable stats End */
+		/*AtpApi api = new AtpApi("/examples/landau/");
+		api.setSimple(0.0,0.0,1.0, "E", 3, 8);
+		api.runAll();*/
+		 /* Copy pasteable foobar */
+        String[] provers = new String[2];
+        provers[0] = "V";
+        provers[1] = "E";
+        PSAPremiseGrowth premiseGrowth = new PSAPremiseGrowth(5, "plus", 10, 5, 0, provers);
+        PSADivvy divvy = new PSADivvy(2, 5, provers, true);
+        PSASimple one = new PSASimple("V",300);
+        PSASimple two = new PSASimple("E",5);
+        /* End Copy pasteable foobar */
 
 
+        /* Start tests */
+		Example example = new Example("Euclid");
+        //example.runAprils();
+        Obligations obligations = example.obligations;
+        //ex.deleteExistentialPremises(obligations);
+        //Default values are weightAPRILS = 1; weightObligationGraph=1;
+		int maxTime = 5 * 1000;
+        obligations.weightAPRILS = 0.0;
+        obligations.weightNaproche = 1.0;
+        obligations.weightObligationGraph = 1.0;
+        obligations.maxTime = maxTime;
+        obligations.threads = 2;
+        //PSA Settings
+        divvy = new PSADivvy(1, 30, provers, true,5);
+        // Log
+        System.out.println("Example: "+example.name);
+        divvy.changeCheckSettings(obligations);
+        obligations.checkObligations();
+        /* Write machine readable stats Start */
+        Statistics.printMachineStats(obligations, "EuclidDivvyEV30S5N1A0.stats");
+        Statistics.printProcessTimeline(obligations, 300,
+"EuclidDivvyEV30S5N1A0.timeline");
+        /* Write machine readable stats End */	
 	}
 	
 	/* Redirect output stream to file */
